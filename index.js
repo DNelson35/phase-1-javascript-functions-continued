0247250495efeda9261072455e1ef7d6cf469e8d
Damien Nelson <dhnisaman@gmail.com>
2022-11-22 18:17:15 -0500

    commit tree 48482fc91891b636c134e4923184bd4f334500d4

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(act = "roller-skate"){
+    return `This Saturday, I want to ${act}!`
+}
+
+const mondayWork = function(act = "go to the office"){
+    return `This Monday, I will ${act}.`
+}
+
+function wrapAdjective(symbol = '*'){
+    return function(message = "special"){
+        return `You are ${symbol}${message}${symbol}!`
+    }
+}
+
